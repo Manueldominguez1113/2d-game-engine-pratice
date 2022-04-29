fdaf4b7bf1bf0cbea551c2e2c8da175e9309c461
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-04-29 18:56:26 -0500

    food break

---
+++ src/com.tutorial.main/Window.java
@@ -0,0 +1,14 @@
+package com.tutorial.main;
+
+import javax.swing.*;
+import java.awt.Canvas;
+
+public class Window extends Canvas {
+
+    public Window(int width, int height, String title){
+        JFrame frame = new JFrame(title);
+
+
+    }
+
+}
