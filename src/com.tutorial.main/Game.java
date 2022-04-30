312be185c680e8fc1a49a017d4f54acc64bf22ed
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-04-30 12:45:39 -0500

    created a window for the game

--- src/com.tutorial.main/Game.java
+++ src/com.tutorial.main/Game.java
@@ -1,8 +1,25 @@
 package com.tutorial.main;
 
-public class Game {
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+
+    public Game() {
+        new Window(WIDTH, HEIGHT, "lets build a game!", this);
+    }
 
     public static void main(String[] args) {
+
+    }
+
+    public synchronized void start() {
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
